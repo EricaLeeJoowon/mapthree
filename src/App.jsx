1fab9158da405d48f62c138984c6d895f99188af
seo1v <165138611+seo1v@users.noreply.github.com>
2024-06-18 16:48:28 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,50 @@
-//App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import MapComponent from './components/MapComponent';
-import BusPage from './pages/BusPage'; // 기존 BusPage 경로
-import SubwayPage from './pages/SubwayPage'; // 기존 SubwayPage 경로
+import Home from './Home.jsx';
+import MapPage from './pages/MapPage.jsx';
+import BusPage from './pages/BusPage.jsx';
+import SubwayPage from './pages/SubwayPage.jsx';
+import ChatbotPage from './pages/ChatbotPage.jsx';
+import SentimentPage from './pages/SentimentPage.jsx';
 import './App.css';
 
 const App = () => {
-    return (
-      <Router>
-        <div className="app-container">
-          <nav className="nav-bar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/map">Map Page</Link>
-              </li>
-              <li>
-                <Link to="/subway">Subway Page</Link>
-              </li>
-              <li>
-                <Link to="/bus">Bus Page</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/map" element={<MapComponent />} />
-            <Route path="/subway" element={<SubwayPage />} />
-            <Route path="/bus" element={<BusPage />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  };
+  return (
+    <Router>
+      <div className="app-container">
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/map">Map Page</Link>
+            </li>
+            <li>
+              <Link to="/subway">Subway Page</Link>
+            </li>
+            <li>
+              <Link to="/bus">Bus Page</Link>
+            </li>
+            <li>
+              <Link to="/chatbot">Chatbot Page</Link>
+            </li>
+            <li>
+              <Link to="/sentiment">Sentiment Analysis</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/subway" element={<SubwayPage />} />
+          <Route path="/bus" element={<BusPage />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+          <Route path="/sentiment" element={<SentimentPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
